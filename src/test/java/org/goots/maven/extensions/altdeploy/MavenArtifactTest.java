f64358087ef5c9e935115d547e59f9617c43fcb9
Nick Cross <ncross@redhat.com>
2020-09-18 15:21:58 +0100

    NCLSUP131 Fix updating all plugins with the project

--- src/test/java/org/goots/maven/extensions/altdeploy/MavenArtifactTest.java
+++ src/test/java/org/goots/maven/extensions/altdeploy/MavenArtifactTest.java
@@ -22,14 +22,13 @@ import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.OverConstrainedVersionException;
 import org.apache.maven.model.Plugin;
+import org.apache.maven.project.MavenProject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
-import javax.management.remote.rmi._RMIConnection_Stub;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -41,16 +40,21 @@ public class MavenArtifactTest
     @Rule
     public final SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
 
-    private Plugin mdp = new Plugin();
+    private MavenProject project1;
+    private Plugin mdp;
 
     @Before
     public void setup()
     {
+        mdp = new Plugin();
         mdp.setGroupId( "org.apache.maven" );
         mdp.setArtifactId( "maven-deploy-plugin" );
         mdp.setVersion( "" );
+
+        project1 = new MavenProject();
+        project1.setGroupId( "project1" );
     }
-    
+
     @Test
     public void compareArtifact() throws OverConstrainedVersionException, IllegalAccessException
     {
@@ -73,9 +77,12 @@ public class MavenArtifactTest
                                                "runtime", "maven-plugin", "", new DefaultArtifactHandler(  ) );
         Map<Artifact, Plugin> collection = new HashMap<>();
         collection.put( legacy, mdp );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
+
 
         AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
-        Set<Artifact> result = spy.processArtifacts( collection );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
 
         assertEquals( 1, result.size() );
         assertTrue ( result.stream().findFirst().get().getVersion().equalsIgnoreCase( "3.0.0-M1" ));
@@ -91,9 +98,11 @@ public class MavenArtifactTest
         Map<Artifact, Plugin> collection = new HashMap<>();
         collection.put( legacy, mdp );
         collection.put( text, mdp );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
 
         AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
-        Set<Artifact> result = spy.processArtifacts( collection );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
 
         assertEquals( 1, result.size() );
         assertTrue ( result.stream().findFirst().get().getVersion().equalsIgnoreCase( legacy.getVersion() ));
@@ -109,9 +118,11 @@ public class MavenArtifactTest
         Map<Artifact, Plugin> collection = new HashMap<>();
         collection.put( text, mdp );
         collection.put( post3M1, mdp );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
 
         AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
-        Set<Artifact> result = spy.processArtifacts( collection );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
 
         assertEquals( 1, result.size() );
         assertTrue ( result.stream().findFirst().get().getVersion().equalsIgnoreCase( post3M1.getVersion() ));
@@ -130,9 +141,11 @@ public class MavenArtifactTest
         collection.put( legacy, mdp );
         collection.put( text, mdp );
         collection.put( post3M1, mdp );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
 
         AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
-        Set<Artifact> result = spy.processArtifacts( collection );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
 
         assertEquals( 2, result.size() );
         assertTrue ( result.stream().findFirst().get().getVersion().equalsIgnoreCase( "3.0.0-M1" ));
@@ -148,11 +161,52 @@ public class MavenArtifactTest
         Map<Artifact, Plugin> collection = new HashMap<>();
         collection.put( legacy, mdp );
         collection.put( post3M1, mdp );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
 
         AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
-        Set<Artifact> result = spy.processArtifacts( collection );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
 
         assertEquals( 2, result.size() );
         assertTrue ( result.stream().findFirst().get().getVersion().equalsIgnoreCase( "3.0.0-M1" ));
+    }
+
+    @Test
+    public void compareArtifactWithMultipleProject()
+    {
+        Artifact legacy = new DefaultArtifact( "org.apache.maven.plugins", "maven-deploy-plugin", "2.8",
+                                               "runtime", "maven-plugin", "", new DefaultArtifactHandler(  ) );
+        Artifact post3M1 = new DefaultArtifact( "org.apache.maven.plugins", "maven-deploy-plugin", "RELEASE",
+                                                "runtime", "maven-plugin", "", new DefaultArtifactHandler(  ) );
+        Artifact legacy2 = new DefaultArtifact( "org.apache.maven.plugins", "maven-deploy-plugin", "2.8",
+                                                "runtime", "maven-plugin", "", new DefaultArtifactHandler(  ) );
+        mdp.setVersion( "2.8" );
+        Plugin mdp2 = mdp.clone();
+        mdp2.setGroupId( "org.apache.maven" );
+        mdp2.setArtifactId( "maven-deploy-plugin" );
+        mdp2.setVersion( "2.8" );
+        Plugin mdp3 = mdp.clone();
+        mdp3.setGroupId( "org.apache.maven" );
+        mdp3.setArtifactId( "maven-deploy-plugin" );
+        mdp3.setVersion( "RELEASE" );
+
+        Map<Artifact, Plugin> collection = new HashMap<>();
+        collection.put( legacy, mdp );
+        collection.put( post3M1, mdp2 );
+        Map<Artifact, Plugin> collection2 = new HashMap<>();
+        collection2.put( legacy2, mdp3 );
+        Map<MavenProject, Map<Artifact, Plugin>> projectPlugins = new HashMap<>();
+        projectPlugins.put( project1, collection );
+        MavenProject project2 = new MavenProject();
+        project2.setGroupId( "project2" );
+        projectPlugins.put( project2, collection2 );
+
+        AltDeployEventSpy spy = new AltDeployEventSpy( new LayoutParser() );
+        Set<Artifact> result = spy.processArtifacts( projectPlugins );
+
+        assertEquals( 1, result.size() );
+        assertTrue ( mdp.getVersion().equalsIgnoreCase( "2.8" ));
+        assertTrue ( mdp2.getVersion().equalsIgnoreCase( "2.8" ));
+        assertTrue ( mdp3.getVersion().equalsIgnoreCase( "2.8" ));
     }
 }
