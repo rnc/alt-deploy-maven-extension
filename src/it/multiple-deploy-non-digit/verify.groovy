7082219e11c54aa5b0814deb180842fa33cc9e2f
Nick Cross <ncross@redhat.com>
2023-05-05 11:13:12 +0100

    Update versions to resolve provision issues (#15)

--- src/it/multiple-deploy-non-digit/verify.groovy
+++ src/it/multiple-deploy-non-digit/verify.groovy
@@ -13,15 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-def pomFile = new File( basedir, 'pom.xml' )
-System.out.println( "Slurping POM: ${pomFile.getAbsolutePath()}" )
-
 
 File buildLog = new File( basedir, 'build.log' )
+System.out.println( "Slurping buildLog: ${buildLog.getAbsolutePath()}" )
+
 assert buildLog.getText().contains( "Activating AltDeploy extension" )
 assert buildLog.getText().contains( "malformed project" )
 assert buildLog.getText().contains( "Found non-digit versions for deploy plugin org.apache.maven.plugins:maven-deploy-plugin:maven-plugin:RELEASE:runtime" )
-assert buildLog.getText().contains( "Found multiple minor versions of maven-deploy-plugin; this is a malformed project" )
-assert buildLog.getText().contains( "maven-deploy-plugin:2.6" )
-assert buildLog.getText().contains( "maven-deploy-plugin:2.7" )
-assert !buildLog.getText().contains( "maven-deploy-plugin:3.0.0-M1" )
+if (mavenVersion.matches("^3.9.*") ) {
+    assert buildLog.getText().contains( "Found multiple minor versions of maven-deploy-plugin; this is a malformed project")
+    assert buildLog.getText().contains( "maven-deploy-plugin:3.0.0" )
+    assert !buildLog.getText().contains( "maven-deploy-plugin:2.6" )
+    assert buildLog.getText().contains( "maven-deploy-plugin:3.0.0-M1" )
+} else {
+    assert buildLog.getText().contains("Found multiple minor versions of maven-deploy-plugin; this is a malformed project")
+    assert buildLog.getText().contains( "maven-deploy-plugin:2.7" )
+    assert buildLog.getText().contains( "maven-deploy-plugin:2.6" )
+    assert !buildLog.getText().contains( "maven-deploy-plugin:3.0.0-M1" )
+}
